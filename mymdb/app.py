dda7c8716147ed6f035e5f275acfe3a9c9b6414e
michzza <srour.michele@gmail.com>
2021-04-30 21:13:58 +0100

    soirée hack

--- mymdb/app.py
+++ mymdb/app.py
@@ -1,23 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import sqlite3
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello" : "World"}
+templates = Jinja2Templates(directory="mymdb/templates")
 
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request, search: str = ""):
+    results = []  
+    if search != "":
+        con = sqlite3.connect('mymdb.db')
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
 
-@app.get("/titles/{title_id}")
-def read_item(title_id):
+        cur.execute("""SELECT * FROM titles 
+            WHERE primary_title LIKE ? 
+            AND type = 'movie'
+            LIMIT 15;""", ['%' + search + '%'])
+        results = cur.fetchall()
+        con.close()
+    return templates.TemplateResponse("home.html", {'request': request, 'search' : search, 'results' : results})
+
+@app.get("/titles/{title_id}", response_class=HTMLResponse)
+def read_item(request: Request, title_id):
     con = sqlite3.connect('mymdb.db')
     con.row_factory = sqlite3.Row
     cur = con.cursor()
 
-
     cur.execute("""SELECT * FROM titles WHERE id = ?;""", [title_id])
     result = cur.fetchone()
     con.close()
-    return result
+    return templates.TemplateResponse("film.html", {'request': request, 'result' : result})
